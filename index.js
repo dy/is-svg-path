c39895898cd4d597ef394d5390943d793c36d426
Dmitry <df.creative@gmail.com>
2017-08-24 00:03:13 +0300

    Enhance numbers detection

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function isPath(str) {
 	str = str.trim()
 
 	// https://www.w3.org/TR/SVG/paths.html#PathDataBNF
-	if (/^[mzlhvcsqta]\s*[0-9][^mlhvzcsqta]+/i.test(str) && /[\dz]$/i.test(str) && str.length > 4) return true
+	if (/^[mzlhvcsqta]\s*[-+.0-9][^mlhvzcsqta]+/i.test(str) && /[\dz]$/i.test(str) && str.length > 4) return true
 
 	return false
 }
